ddd976a33c75b884d7fb5b2fbfc555a7c4ba86a9
c30net <c30net@gmail.com>
2021-09-09 23:49:59 +0330

    create, read, delete was done

--- js/app.js
+++ js/app.js
@@ -4,18 +4,51 @@ const httpForm = document.getElementById("httpForm");
 const itemInput = document.getElementById("itemInput");
 const imageInput = document.getElementById("imageInput");
 const feedback = document.querySelector(".feedback");
-const items = document.querySelector(".items");
-const submtiBtn = document.getElementById("submitBtn");
+//const items = document.querySelector(".items");
+const submitBtn = document.getElementById("submitBtn");
 let editedItemID = 0;
 const url = 'https://6130d77b8066ca0017fdaac2.mockapi.io/items';
+
+
+submitBtn.addEventListener('click', submitItem);
+//submit item
+ function submitItem(event){
+     event.preventDefault();
+     const itemValue = itemInput.value;
+     const imageValue = imageInput.value;
+     if(itemValue.length === 0 || imageValue.length === 0){
+         showFeedback('please inter valid values');
+     } else {
+        postItemAPI(itemValue, imageValue);
+         itemInput.value = '';
+         imageInput.value = '';
+     }
+ }
+
+
+
+
 
 //load items
 
 document.addEventListener('DOMContentLoaded', function (){
-    //fir a function of get items API
+    //fire a function of get items API
     getItemsAPI(showItems);
     //callback function
 })
+
+//show feedback
+
+function showFeedback (text){
+     feedback.classList.remove('feedback');
+     feedback.innerHTML = `<span>${text}</span>`;
+     setTimeout(function(){
+         feedback.classList.add('feedback');
+     }, 3000)
+
+}
+
+
 
 //get items
 
@@ -39,9 +72,7 @@ function getItemsAPI(cb){
 function showItems(data){
     let info = '';
     const items = JSON.parse(data);
-    console.log(items);
     items.forEach( item => {
-        console.log(item);
         info += `<!-- single item -->
 
                     <li class="list-group-item d-flex align-items-center justify-content-between flex-wrap item my-2">
@@ -52,7 +83,7 @@ function showItems(data){
                             <a href='#' class="itemIcon mx-2 edit-icon" data-id='${item.id}'>
                                 <i class="fas fa-edit"></i>
                             </a>
-                            <a href='#' class="itemIcon mx-2 delete-icon" data-id=''>
+                            <a href='#' class="itemIcon mx-2 delete-icon" data-id='${item.id}'>
                                 <i class="fas fa-trash"></i>
                             </a>
                         </div>
@@ -60,4 +91,77 @@ function showItems(data){
                     <!-- end of single item -->`;
     })
        itemList.innerHTML = info;
+        //get icons
+        getIcons();
+}
+
+//post items
+
+function postItemAPI (item, image) {
+    const avatar = `img/${image}.jpeg`;
+    const name = item;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function (){
+        getItemsAPI(showItems);
+    }
+    xhr.onerror = function(){
+        console.log('There is an error');
+    }
+
+    xhr.send(`avatar=${avatar}&name=${name}`);
+}
+
+function getIcons(){
+     const editIcon = Array.from(document.querySelectorAll('.edit-icon'));
+     const deleteIcon = Array.from(document.querySelectorAll('.delete-icon'));
+    deleteIcon.forEach(function(icon) {
+        const iconID = icon.getAttribute('data-id');
+        icon.addEventListener('click', function(event){
+            event.preventDefault();
+            console.log(iconID);
+            deleteItemAPI(iconID);
+        })
+    })
+    editIcon.forEach(function(icon) {
+        const iconID = icon.getAttribute('data-id');
+        icon.addEventListener('click', function(event){
+            event.preventDefault();
+            const parent = event.target.parentElement.parentElement.parentElement;
+            const img = parent.querySelector('.itemImage').src;
+            const name = parent.querySelector('.itemName').textContent;
+            console.log(img, name, iconID);
+            editItem(parent, img, name, iconID);
+        })
+    })
+
+}
+
+function editItemAPI(id){
+
+}
+
+function deleteItemAPI(id){
+     const deleteUrl = url+'/' +id;
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function (){
+        if(xhr.status === 200) {
+            console.log(xhr.responseText);
+            getItemsAPI(showItems);
+        }
+    }
+    xhr.onerror = function(){
+        console.log('There is an error');
+    }
+    xhr.open('DELETE', deleteUrl, true);
+    xhr.send();
+
+}
+
+function editItem(parent, itemImg, name, iconID){
+    event.preventDefault();
+    itemList.removeChild(parent);
+
 }
